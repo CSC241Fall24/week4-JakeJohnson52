ef66da34ac6930fbbc6ef6f282506c97e85859fe
JakeJohnson52 <157393198+JakeJohnson52@users.noreply.github.com>
2024-09-19 12:57:26 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -5,8 +5,10 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        for(int i = 0; i < l2.size(); i++)    
-               l1.add(l2.get(i));
+        for(int i = 0; i < l2.size(); i++){    
+            ListNode temp = new ListNode(l2.get(i));
+            l1.add(temp);
+        }
         return l1;
     }
 }
