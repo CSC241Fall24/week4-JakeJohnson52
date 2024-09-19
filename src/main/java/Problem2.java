6940a4e3a689d6a7673910fac2b011ebd2806f59
JakeJohnson52 <157393198+JakeJohnson52@users.noreply.github.com>
2024-09-19 16:51:08 -0400

    Update Problem2.java

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -4,13 +4,8 @@ public class Problem2 {
         // TODO: Implement the insert method
         // This method should insert a new node with the given value at the specified position
         // If the position is out of bounds, insert the node at the end of the list
-        if(head.size() <= position){
-            ListNode a = new ListNode(val);
-            head.next = a;
-        }else{
-            ListNode b = new ListNode(val);
-            head.insert(position -1, b);
-        }
+        ListNode a = new ListNode(val);
+        head.next = a;
         return head;
     }
 }
