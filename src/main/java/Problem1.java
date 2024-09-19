dd8ebbdb8351370562aa583c9b02308b5f213893
JakeJohnson52 <157393198+JakeJohnson52@users.noreply.github.com>
2024-09-19 13:01:12 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -7,7 +7,7 @@ public class Problem1 {
         // Remember to create new nodes instead of directly linking l1 to l2
         for(int i = 0; i < l2.size(); i++){    
             ListNode temp = new ListNode(l2.get(i));
-            l1.next() = temp;
+            l1.next = temp;
         }
         return l1;
     }
